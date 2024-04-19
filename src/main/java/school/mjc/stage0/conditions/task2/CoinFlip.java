30043ce0f4e01c8b1671836b1c448930c60f51bd
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-04-19 23:07:50 +0500

    Edited

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task2;
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
         if(from1UpTo1000 < 500){
-            System.out.println("Edge");
+            System.out.println("Eagle");
         } else {
             System.out.println("Tail");
         }
